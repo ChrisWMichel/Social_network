2c7350e3864e1b5b3b533a4709a1d0010cca6f11
Chris Michel <christopherw.michel@gmail.com>
2018-03-28 16:11:43 -0500

    Added Notification

--- web/assets/js/custom/common.js
+++ web/assets/js/custom/common.js
@@ -1,36 +1,4 @@
 $(document).ready(function () {
-    /*let ias = jQuery.ias({
-        container: '#timeline .box-content',
-        item: '.publication-item',
-        pagination: '#timeline .pagination',
-        next: '#timeline .pagination .next_link',
-        triggerPageThreshold: 5
-    });
-
-    ias.extension(new IASTriggerExtension({
-        text: 'Veiw more',
-        offset: 3
-    }));
-
-    /!*ias.extension(new IASSpinnerExtension({
-        src: URL+'../assets/images/ajax-loader.gif'
-    }));*!/
-    ias.extension(new IASSpinnerExtension({
-        /!*src: "{{ asset('assets/images/ajax-loader.gif') }}"*!/
-        src: image_path
-    }));
-
-    ias.extension(new IASNoneLeftExtension({
-        text: 'There are no more posts.'
-    }));
-
-    ias.on('ready',function (event) {
-        Buttons();
-    });
-
-    ias.on('rendered',function (event) {
-        Buttons();
-    });*/
 
     if($(".label-notification").text() === 0){
         $(".label-notification").addClass("hidden");
@@ -51,7 +19,7 @@ $(document).ready(function () {
             success: function (response) {
                 $(".label-notification").html(response.count) ;
 
-                if($(response.count).text() === 0){
+                if((response.count) === 0){
                     $(".label-notification").addClass("hidden");
                 }else{
                     $(".label-notification").removeClass("hidden");
